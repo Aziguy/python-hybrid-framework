6e4859322896661ee0777bf1378c2282cbd682a6
Aziguy <monsieur_hernandez@hotmail.com>
2024-07-24 17:21:18 +0200

    Create a utils file for utilities functions.

--- tests/test_loging.py
+++ tests/test_loging.py
@@ -1,16 +1,10 @@
-from datetime import datetime
-
 import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
+from utils.utils import generate_email_time_stamp
 
 
 @pytest.mark.usefixtures('setup_teardown')
 class TestLoging:
-    @staticmethod
-    def generate_email_time_stamp():
-        time_stamp = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
-        return f"aziguy{time_stamp}@gmail.com"
 
     def test_login_with_valid_credentials(self):
         self.driver.find_element(By.XPATH, "//span[text()='My Account']").click()
@@ -23,7 +17,7 @@ class TestLoging:
     def test_login_with_invalid_email_and_valid_pwd(self):
         self.driver.find_element(By.XPATH, "//span[text()='My Account']").click()
         self.driver.find_element(By.LINK_TEXT, "Login").click()
-        self.driver.find_element(By.ID, "input-email").send_keys(self.generate_email_time_stamp())
+        self.driver.find_element(By.ID, "input-email").send_keys(generate_email_time_stamp())
         self.driver.find_element(By.ID, "input-password").send_keys("12345")
         self.driver.find_element(By.XPATH, "//input[@value='Login']").click()
         expected_warning_message = "Warning: No match for E-Mail Address and/or Password."
